de18e2e6203354a2dd65ba35b8ca97c80f0530cd
José Erivam <erivamdev@gmail.com>
2022-06-24 10:39:07 -0300

    :sparkles: feat:add model BookService

--- src/test/java/com/joseerivam/libaryapi/BookControllerTest.java
+++ src/test/java/com/joseerivam/libaryapi/BookControllerTest.java
@@ -3,9 +3,12 @@ package com.joseerivam.libaryapi;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -15,6 +18,8 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.joseerivam.libaryapi.api.dto.BookDTO;
+import com.joseerivam.libaryapi.model.Book;
+import com.joseerivam.libaryapi.service.BookService;
 
 @ExtendWith(SpringExtension.class)
 @ActiveProfiles("test")
@@ -27,17 +32,25 @@ public class BookControllerTest {
   @Autowired
   MockMvc mvc;
 
+  @MockBean
+  BookService service;
+
   @Test
   @DisplayName("Must successfully create a book")
   public void createBookTest() throws Exception {
+
     BookDTO dto = BookDTO.builder().author("Arthur").title("As Aventuras").isbn("001").build();
+    Book savedBook =
+        Book.builder().id(10L).author("Arthur").title("As Aventuras").isbn("001").build();
+
+    BDDMockito.given(service.save(Mockito.any(Book.class))).willReturn(savedBook);
     String json = new ObjectMapper().writeValueAsString(dto);
 
     MockHttpServletRequestBuilder request = MockMvcRequestBuilders.post(BOOK_API)
         .contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON).content(json);
 
     mvc.perform(request).andExpect(MockMvcResultMatchers.status().isCreated())
-        .andExpect(MockMvcResultMatchers.jsonPath("id").isNotEmpty())
+        .andExpect(MockMvcResultMatchers.jsonPath("id").value(10L))
         .andExpect(MockMvcResultMatchers.jsonPath("title").value(dto.getTitle()))
         .andExpect(MockMvcResultMatchers.jsonPath("author").value(dto.getAuthor()))
         .andExpect(MockMvcResultMatchers.jsonPath("isbn").value(dto.getIsbn()));
