1a63c38279c65f7bcfbed9a6349c1831745dac24
José Erivam <erivamdev@gmail.com>
2022-06-24 10:38:01 -0300

    :recycle: refactor:controller create

--- src/main/java/com/joseerivam/libaryapi/api/resource/BookController.java
+++ src/main/java/com/joseerivam/libaryapi/api/resource/BookController.java
@@ -8,15 +8,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import com.joseerivam.libaryapi.api.dto.BookDTO;
+import com.joseerivam.libaryapi.model.Book;
+import com.joseerivam.libaryapi.service.BookService;
 
 @RestController
 @RequestMapping("/api/books")
 public class BookController {
 
+  private BookService service;
+
+  public BookController(BookService service) {
+    this.service = service;
+  }
+
+
+
   @PostMapping
   @ResponseStatus(HttpStatus.CREATED)
   public BookDTO create(@RequestBody BookDTO dto) {
-    return dto;
+    Book entity =
+        Book.builder().author(dto.getAuthor()).title(dto.getTitle()).isbn(dto.getIsbn()).build();
+    entity = service.save(entity);
+
+    return BookDTO.builder().id(entity.getId()).author(entity.getAuthor()).title(entity.getTitle())
+        .isbn(entity.getIsbn()).build();
   }
 
 }
