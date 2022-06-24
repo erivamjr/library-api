a2ffaaf0d10d5195bffd021c4b2dfc8172af18e8
José Erivam <erivamdev@gmail.com>
2022-06-24 10:35:48 -0300

    :sparkles: feat:add entity dto for to help test

--- src/main/java/com/joseerivam/libaryapi/api/dto/BookDTO.java
+++ src/main/java/com/joseerivam/libaryapi/api/dto/BookDTO.java
@@ -2,12 +2,10 @@ package com.joseerivam.libaryapi.api.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
+@Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
