e57589f437e79962d93175c4497089f377f15b5d
José Erivam <erivamdev@gmail.com>
2022-09-22 14:14:50 -0300

    :bug: fix:repair dependecies spring boot version 2.5.14

--- src/main/java/com/joseerivam/libaryapi/config/SwaggerConfig.java
+++ src/main/java/com/joseerivam/libaryapi/config/SwaggerConfig.java
@@ -2,6 +2,9 @@ package com.joseerivam.libaryapi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -11,9 +14,10 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+
 @EnableSwagger2
 @Configuration
-public class SwaggerConfig {
+public class SwaggerConfig implements WebMvcConfigurer {
 
     @Bean
     public Docket docket() {
